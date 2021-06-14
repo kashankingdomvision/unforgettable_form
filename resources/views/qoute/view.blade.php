777610775d45b71af006abfe37e30af71bbd8e1c
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-06-14 16:45:04 +0500

    Update view.blade.php
    hidden booking button on quote
    
    Co-Authored-By: kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>

--- resources/views/qoute/view.blade.php
+++ resources/views/qoute/view.blade.php
@@ -50,13 +50,13 @@
                         <td>{{ $quote->sale_person }}</td>
                         <td>{{ $quote->currency }}</td>
                         <td>{{ $quote->group_no }}</td>
-                        <td>
+                        <td width="10%">
                         <a href="{{ URL::to('edit-quote/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class="fa fa-pencil"></span></a>
 
-                        <a onclick="return confirm('Are you sure you want to convert this Quotation to Booking?');" href="{{ route('convert-quote-to-booking', $quote->id) }}" class="btn btn-primary btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-check"></span></a>
-                        <a href="{{ URL::to('confirm-booking/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class=""></span>Booking</a>
+                        <a onclick="return confirm('Are you sure you want to convert this Quotation to Booking?');" href="{{ route('convert-quote-to-booking', $quote->id) }}" class="btn btn-success btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-check"></span></a>
+                        {{-- <a href="{{ URL::to('confirm-booking/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class=""></span>Booking</a> --}}
                         
-                        <a onclick="return confirm('Are you sure want to Delete {{ $quote->ref_no }}');" href="{{ route('delete-quote', encrypt($quote->id)) }}" class="btn btn-primary btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-remove"></span></a>
+                        <a onclick="return confirm('Are you sure want to Delete {{ $quote->ref_no }}');" href="{{ route('delete-quote', encrypt($quote->id)) }}" class="btn btn-danger btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-trash"></span></a>
                         </td>
                     </tr>
                 @endforeach
