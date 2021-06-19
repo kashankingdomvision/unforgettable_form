701c55bbffd47ebcaf0d37708c3dee34e142f487
kashankingdomvision <59564656+kashankingdomvision@users.noreply.github.com>
2021-06-19 14:01:24 +0500

    some fixes

--- resources/views/qoute/view.blade.php
+++ resources/views/qoute/view.blade.php
@@ -52,24 +52,21 @@ tr.shown td.details-control {
                 </thead>
               
                 @foreach ($quotes as $key => $quote)
-                    <tr>
-                        {{-- <td>{{ $key+1 }}</td> --}}
-                        <td>{{ $quote->ref_no }}</td>
-                        <td>{{ $quote->season->name }}</td>
-                        <td>{{ $quote->type_of_holidays }}</td>
-                        <td>{{ $quote->brand_name }}</td>
-                        <td>{{ $quote->sale_person }}</td>
-                        <td>{{ $quote->currency }}</td>
-                        <td>{{ $quote->group_no }}</td>
-                        <td width="10%">
+                  <tr>
+                      {{-- <td>{{ $key+1 }}</td> --}}
+                      <td>{{ $quote->ref_no }}</td>
+                      <td>{{ !empty($quote->season->name) ? $quote->season->name : '' }}</td>
+                      <td>{{ $quote->type_of_holidays }}</td>
+                      <td>{{ $quote->brand_name }}</td>
+                      <td>{{ $quote->sale_person }}</td>
+                      <td>{{ $quote->currency }}</td>
+                      <td>{{ $quote->group_no }}</td>
+                      <td width="10%">
                         <a href="{{ URL::to('edit-quote/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class="fa fa-pencil"></span></a>
-
                         <a onclick="return confirm('Are you sure you want to convert this Quotation to Booking?');" href="{{ route('convert-quote-to-booking', $quote->id) }}" class="btn btn-success btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-check"></span></a>
-                        {{-- <a href="{{ URL::to('confirm-booking/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class=""></span>Booking</a> --}}
-                        
                         <a onclick="return confirm('Are you sure want to Delete {{ $quote->ref_no }}');" href="{{ route('delete-quote', encrypt($quote->id)) }}" class="btn btn-danger btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-trash"></span></a>
-                        </td>
-                    </tr>
+                      </td>
+                  </tr>
                 @endforeach
             </table>
             </div>
