53ac80ede9be348559615a953701d26d89e6cbf2
Tabraiz-bukhari <84439748+Tabraiz-bukhari@users.noreply.github.com>
2021-06-23 12:09:23 +0500

    Update partial_quote_child.blade.php
    view quote

--- resources/views/qoute/partial_quote_child.blade.php
+++ resources/views/qoute/partial_quote_child.blade.php
@@ -16,11 +16,17 @@
   <td>{{ $quote->currency }}</td>
   <td>{{ $quote->group_no }}</td>
       <td>{!! $quote->booking_formated_status !!}</td>
-      <td>{{ $quote->qoute_to_booking_date??NULL }}</td>
-    <td width="10%">
-    <a href="{{ URL::to('edit-quote/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class="fa fa-pencil"></span></a>
+      <td>{{ date('d/m/Y', strtotime($quote->qoute_to_booking_date)) ??NULL }}</td>
 
-    <a onclick="return confirm('Are you sure you want to convert this Quotation to Booking?');" href="{{ route('convert-quote-to-booking', $quote->id) }}" class="btn btn-success btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-check"></span></a>
+    <td width="10%">
+      @if($quote->qoute_to_booking_status == 0)
+        <a href="{{ URL::to('edit-quote/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class="fa fa-pencil"></span></a>
+        <a onclick="return confirm('Are you sure you want to convert this Quotation to Booking?');" href="{{ route('convert-quote-to-booking', $quote->id) }}" class="btn btn-success btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-check"></span></a>
+      @endif
+      
+      @if($quote->qoute_to_booking_status == 1)
+      <a target="_blank" href="{{ route('view-quote-detail', $quote->id) }}" class="btn btn-primary btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-eye"></span></a>
+      @endif
     {{-- <a href="{{ URL::to('confirm-booking/'.$quote->id)}}" class="btn btn-primary btn-xs" data-title="Edit" data-target="#edit"><span class=""></span>Booking</a> --}}
     
     <a onclick="return confirm('Are you sure want to Delete {{ $quote->ref_no }}');" href="{{ route('delete-quote', encrypt($quote->id)) }}" class="btn btn-danger btn-xs" data-title="Delete" data-target="#delete"><span class="fa fa-trash"></span></a>
